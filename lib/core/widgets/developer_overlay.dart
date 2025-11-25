f8d462702ebfb21eb433bde2dc5d1ec9ef7267cc
3oraby <mohamedahmed21102003@gmail.com>
2025-11-25 16:36:32 +0200

    add translation button

--- lib/core/widgets/developer_overlay.dart
+++ lib/core/widgets/developer_overlay.dart
@@ -1,3 +1,4 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:san3a_app/core/cubits/app_theme_cubit/app_theme_cubit.dart';
@@ -13,6 +14,21 @@ class DeveloperOverlay extends StatelessWidget {
     return Stack(
       children: [
         child,
+        Positioned(
+          bottom: 50,
+          left: 16,
+          child: FloatingActionButton(
+            mini: true,
+            onPressed: () async {
+              await context.setLocale(
+                context.locale == const Locale('en')
+                    ? const Locale('ar')
+                    : const Locale('en'),
+              );
+            },
+            child: const Icon(Icons.language),
+          ),
+        ),
         Positioned(
           bottom: 50,
           right: 16,
