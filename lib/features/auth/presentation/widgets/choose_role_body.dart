c88e0b6a442000d0e772b46ef3d38eee03903de0
3oraby <mohamedahmed21102003@gmail.com>
2025-12-13 23:51:32 +0200

    refactor code

--- lib/features/auth/presentation/widgets/choose_role_body.dart
+++ lib/features/auth/presentation/widgets/choose_role_body.dart
@@ -1,12 +1,10 @@
-import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:san3a_app/core/constants/locale_keys.dart';
 import 'package:san3a_app/core/constants/storage_keys.dart';
 import 'package:san3a_app/core/enums/role.dart';
 import 'package:san3a_app/core/helpers/app_storage_helper.dart';
-import 'package:san3a_app/core/utils/app_text_styles.dart';
 import 'package:san3a_app/core/widgets/vertical_gap.dart';
+import 'package:san3a_app/features/auth/presentation/widgets/user_role_card.dart';
 
 class ChooseRoleBody extends StatefulWidget {
   const ChooseRoleBody({super.key, this.onRoleSelected});
@@ -45,59 +43,6 @@ class _ChooseRoleBodyState extends State<ChooseRoleBody> {
           onTap: () => onRoleSelected(Role.craftsman),
         ),
       ],
-    );
-  }
-}
-
-class UserRoleCard extends StatelessWidget {
-  const UserRoleCard({
-    super.key,
-    required this.role,
-    required this.description,
-    required this.onTap,
-  });
-
-  final String role;
-  final String description;
-  final VoidCallback onTap;
-
-  @override
-  Widget build(BuildContext context) {
-    return InkWell(
-      onTap: onTap,
-      child: Container(
-        height: 215.h,
-        width: 227.w,
-        decoration: BoxDecoration(
-          color: Theme.of(context).cardColor,
-          borderRadius: BorderRadius.circular(16.r),
-        ),
-        child: Column(
-          children: [
-            const VerticalGap(48),
-            Text(
-              role.tr(),
-              textAlign: TextAlign.center,
-              style: AppTextStyles.getTextStyle(32).copyWith(
-                fontWeight: FontWeight.w700,
-                color: Theme.of(context).colorScheme.onSurface,
-              ),
-            ),
-            const VerticalGap(24),
-            RPadding(
-              padding: const EdgeInsets.symmetric(horizontal: 42),
-              child: Text(
-                description.tr(),
-                textAlign: TextAlign.center,
-                style: AppTextStyles.getTextStyle(
-                  16,
-                ).copyWith(color: Theme.of(context).colorScheme.onSurface),
-              ),
-            ),
-            const VerticalGap(39),
-          ],
-        ),
-      ),
     );
   }
 }
